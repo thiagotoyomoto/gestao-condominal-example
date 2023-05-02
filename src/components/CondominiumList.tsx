2a512516b1a9ead4c8a50b9a8f1a623dbf5b0bb5
Thiago Toyomoto <contact@thiagotoyomoto.com>
2023-05-01 22:44:25 -0300

    Add missing key prop

--- src/components/CondominiumList.tsx
+++ src/components/CondominiumList.tsx
@@ -9,8 +9,8 @@ export default function CondominiumList({ condominiums }: Props) {
         <nav className='mt-2'>
           <ul className='nav nav-pills nav-sidebar flex-column' data-widget='treeview' role='menu'>
             {condominiums.map((condominium) => (
-              <li className='nav-item'>
-                <a key={`condominium-${condominium.id}`} href={`/empresas/${condominium.company_id}/condominios/${condominium.id}`} className='nav-link'>
+              <li key={`condominium-${condominium.id}`} className='nav-item'>
+                <a href={`/empresas/${condominium.company_id}/condominios/${condominium.id}`} className='nav-link'>
                   <p>{condominium.name}</p>
                   <span className="right badge bg-purple rounded-pill px-2">0</span>
                 </a>
